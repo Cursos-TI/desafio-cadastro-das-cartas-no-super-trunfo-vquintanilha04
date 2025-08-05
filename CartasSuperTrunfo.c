7d0f011bd44f0ec5600e731de9263f3ce4edc22d
vquintanilha04 <viniquintanilha2405@gmail.com>
2025-08-05 19:11:44 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,54 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Carta do jogador
+    char nomePlayer[] = "Godwynn";
+    int forcaPlayer = 90;
+    int inteligenciaPlayer = 70;
+    int agilidadePlayer = 75;
 
+    // Carta do computador
+    char nomePC[] = "Rennala";
+    int forcaPC = 50;
+    int inteligenciaPC = 90;
+    int agilidadePC = 75; 
+
+    int escolha, valorPlayer, valorPC;
+
+    printf("Você é o %s\n", nomePlayer);
+    printf("Escolha o atributo para competir: \n");
+    printf("1 - Força\n2 - Inteligência\n3 - Agilidade\n");
+    scanf("%d", &escolha);
+
+    if (escolha == 1) {
+        valorPlayer = forcaPlayer;
+        valorPC = forcaPC;
+        printf("\nForça da Rennala: %d\n", valorPC);
+    }
+    else if (escolha == 2) {
+        valorPlayer = inteligenciaPlayer;
+        valorPC = inteligenciaPC;
+        printf("\nInteligência da Rennala: %d\n", valorPC);
+    }
+    else if (escolha == 3) {
+        valorPlayer = agilidadePlayer;
+        valorPC = agilidadePC;
+        printf("\nAgilidade da Rennala: %d\n", valorPC);
+    }
+    else {
+        printf("Opção inválida!\n");
+        return 1;
+    }
+
+    if (valorPlayer > valorPC) {
+        printf("\nVocê venceu!");
+    }
+    else if (valorPlayer < valorPC) {
+        printf("\nVocê Perdeu!");
+    }
+    else {
+        printf("\nEmpate!\n");
+    }
+    
     return 0;
 }
