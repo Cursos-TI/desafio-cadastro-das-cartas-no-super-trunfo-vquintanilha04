bcc026423cf7f301b0b03704475ae025990c1ef8
vquintanilha04 <viniquintanilha2405@gmail.com>
2025-08-29 15:38:34 -0300

    Update xadrez.c
    Esse foi bem mais divertido e bem menos estressante que programar o super trunfo hahah

--- xadrez.c
+++ xadrez.c
@@ -2,30 +2,57 @@
 
 int main() {
 
-    int i, torre, bispo, rainha;
+ #include <stdio.h>
 
-    printf("Movimento da Torre (5 casas para a direita):\n");
-    for (torre = 0; torre < 5; torre++) {
-        printf("Direita\n");
-    }
+int main() {
+    int i;
+    int escolha;
+    
+    printf("***MOVIMENTO DE PEÇAS DE XADREZ ***\n");
+    printf("Escolha a peça para mover:\n");
+    printf("1 - Torre\n");
+    printf("2 - Bispo\n");
+    printf("3 - Cavalo\n");
+    printf("4 - Rainha\n");
+    printf("Digite sua escolha (1-4):");
+    scanf("%d", &escolha);
     
     printf("\n");
     
-    printf("Movimento do Bispo (5 casas na diagonal cima/direita):\n");
-    bispo = 0;
-    while (bispo < 5) {
-        printf("Cima, Direita\n");
-        bispo++;
+    switch(escolha) {
+        case 1: 
+            printf("Movimento da Torre (5 casas para a direita):\n");
+            for(i = 0; i < 5; i++) {
+                printf("Direita\n");
+            }
+            break;
+            
+        case 2: 
+            printf("Movimento do Bispo (5 casas na diagonal superior direita):\n");
+            for(i = 0; i < 5; i++) {
+                printf("Cima, Direita\n");
+            }
+            break;
+            
+        case 3: 
+            printf("Movimentação do Cavalo (2 casas para baixo e uma para a esquerda):\n");
+            for(i = 0; i < 2; i++) {
+                printf("Baixo\n");
+            }
+            printf("Esquerda\n");
+            break;
+            
+        case 4:
+            printf("Movimento da Rainha (8 casas para a esquerda):\n");
+            for(i = 0; i < 8; i++) {
+                printf("Esquerda\n");
+            }
+            break;
+            
+        default:
+            printf("Opção inválida!\n");
+            break;
     }
-    
-    printf("\n"); 
-
-    printf("Movimento da Rainha (8 casas para a esquerda):\n");
-    rainha = 0;
-    do {
-        printf("Esquerda\n");
-        rainha++;
-    } while (rainha < 8);
     
     return 0;
 }
