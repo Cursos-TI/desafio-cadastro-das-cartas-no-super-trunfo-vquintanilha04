c3426c4f33fa42040649be79ae6d038b2ea5c97f
vquintanilha04 <viniquintanilha2405@gmail.com>
2025-09-02 17:39:34 -0300

    Update xadrez.c
    Apesar de muito mais linha, não acho que tenha muita diferença no terminal.

--- xadrez.c
+++ xadrez.c
@@ -1,58 +1,111 @@
 #include <stdio.h>
+#include <time.h>
 
-int main() {
+void mtorre(int casas) {
+    if (casas > 0) {
+        printf("Direita\n");
+        mtorre(casas - 1);
+    }
+}
 
- #include <stdio.h>
+void mbispo(int casas) {
+    if (casas > 0) {
+        printf("Cima, Direita\n");
+        mbispo(casas - 1);
+    }
+}
 
-int main() {
-    int i;
-    int escolha;
+void mrainha(int casas) {
+    if (casas > 0) {
+        printf("Esquerda\n");
+        mrainha(casas - 1);
+    }
+}
+
+void mcavalo() {
+    int i, j;
+    int casas_cima = 2;    
+    int casas_direita = 1; 
     
-    printf("***MOVIMENTO DE PEÇAS DE XADREZ ***\n");
+    printf("Movimentação do Cavalo (2 casas para cima e 1 para a direita):\n");
+    
+    for (i = 0; i < casas_cima; i++) {
+      
+        if (i >= casas_cima) {
+            break;  
+        }
+        
+        printf("Cima\n");
+        
+        for (j = 0; j < casas_direita; j++) {
+        
+            if (i == casas_cima - 1) {
+                printf("Direita\n");
+            } else {
+                continue;
+            }
+        }
+    }
+}
+
+void mbispoloops() {
+    int casas = 5;
+    int i, j;
+    
+    printf("\nMovimento do Bispo com loops aninhados (%d casas na diagonal):\n", casas);
+    
+    for (i = 0; i < casas; i++) {
+        
+        for (j = 0; j < casas; j++) {
+            
+            if (i == j) {
+                printf("Cima, Direita\n");
+                break;  
+            }
+        }
+    }
+}
+
+int main() {
+    int escolha;
+    const int casas_torre = 5; 
+    const int casas_bispo = 5;      
+    const int casas_rainha = 8;     
+    printf("*** MOVIMENTO DE PEÇAS DE XADREZ ***\n");
     printf("Escolha a peça para mover:\n");
     printf("1 - Torre\n");
     printf("2 - Bispo\n");
     printf("3 - Cavalo\n");
     printf("4 - Rainha\n");
-    printf("Digite sua escolha (1-4):");
+    printf("Digite sua escolha (1-4): ");
     scanf("%d", &escolha);
     
     printf("\n");
     
     switch(escolha) {
         case 1: 
-            printf("Movimento da Torre (5 casas para a direita):\n");
-            for(i = 0; i < 5; i++) {
-                printf("Direita\n");
-            }
+            printf("Movimento da Torre (%d casas para a direita):\n", casas_torre);
+            mtorre(casas_torre);
             break;
             
         case 2: 
-            printf("Movimento do Bispo (5 casas na diagonal superior direita):\n");
-            for(i = 0; i < 5; i++) {
-                printf("Cima, Direita\n");
-            }
+            printf("Movimento do Bispo (%d casas na diagonal superior direita):\n", casas_bispo);
+            mbispo(casas_bispo);
+            mbispoloops();
             break;
             
         case 3: 
-            printf("Movimentação do Cavalo (2 casas para baixo e uma para a esquerda):\n");
-            for(i = 0; i < 2; i++) {
-                printf("Baixo\n");
-            }
-            printf("Esquerda\n");
+            mcavalo();
             break;
             
         case 4:
-            printf("Movimento da Rainha (8 casas para a esquerda):\n");
-            for(i = 0; i < 8; i++) {
-                printf("Esquerda\n");
-            }
+            printf("Movimento da Rainha (%d casas para a esquerda):\n", casas_rainha);
+            mrainha(casas_rainha);
             break;
             
         default:
             printf("Opção inválida!\n");
             break;
     }
-    
     return 0;
 }
